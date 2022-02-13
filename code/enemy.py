9c7c6fc0fa6730e73ac5b1e6ffa60374f576ad84
John Ayling <john.ayling@digintent.com>
2022-02-13 13:10:51 -0600

    Add enemy attack animations.

--- code/enemy.py
+++ code/enemy.py
@@ -1,4 +1,5 @@
 from turtle import distance
+from numpy import False_
 import pygame
 from settings import *
 from entity import Entity
@@ -25,6 +26,9 @@ class Enemey(Entity):
 		self.attack_radius = monster_info['attack_radius']
 		self.notice_radius = monster_info['notice_radius']
 		self.attack_type = monster_info['attack_type']
+		self.can_attack = True
+		self.attack_time = None
+		self.attack_cooldown = 400
 
 	def import_graphics(self):
 		self.animations = {
@@ -50,11 +54,12 @@ class Enemey(Entity):
 
 		return (distance, direction)
 
-
 	def get_status(self, player):
 		distance = self.get_player_distance_direction(player)[0]
 
-		if distance <= self.attack_radius:
+		if distance <= self.attack_radius and self.can_attack:
+			if self.status != 'attack':
+				self.frame_index = 0
 			self.status = 'attack'
 		elif distance <= self.notice_radius:
 			self.status = 'move'
@@ -63,14 +68,31 @@ class Enemey(Entity):
 
 	def actions(self, player):
 		if self.status == 'attack':
-			pass
+			self.attack_time = pygame.time.get_ticks()
 		elif self.status == 'move':
 			self.direction = self.get_player_distance_direction(player)[1]
 		else:
 			self.direction = pygame.math.Vector2()
 
+	def animate(self):
+		animation = self.animations[self.status]
+		self.frame_index = (self.frame_index + self.animation_speed) % len(animation)
+
+		if (self.frame_index + self.animation_speed >= len(animation) and self.status == 'attack'):
+			self.can_attack = False
+
+		self.image = animation[int(self.frame_index)]
+		self.rect = self.image.get_rect(center = self.hitbox.center)
+
+	def cooldowns(self):
+		if not self.can_attack:
+			current_time = pygame.time.get_ticks()
+			if current_time - self.attack_time >= self.attack_cooldown:
+				self.can_attack = True
+
 	def enemy_update(self, player):
 		self.actions(player)
 		self.get_status(player)
 		self.animate()
+		self.cooldowns()
 		self.move()
